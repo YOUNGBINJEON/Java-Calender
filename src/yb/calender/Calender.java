8556115d8840161953ddac2ffaeaf532e3417887
YOUNGBINJEON <sh44972@gmail.com>
2021-05-11 23:21:32 +0900

    [Update] using class method

--- src/yb/calender/Calender.java
+++ src/yb/calender/Calender.java
@@ -2,9 +2,15 @@ package yb.calender;
 
 import java.util.Scanner;
 
-public class Calebder {
-
-	public static void main(String[] args) {
+public class Calender {
+	
+	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1 ];
+	}
+	
+	public void printSampleCalender() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
@@ -12,13 +18,19 @@ public class Calebder {
 		System.out.println(" 15 16 17 18 19 20 21");
 		System.out.println(" 22 23 24 25 26 27 28");
 		
+	}
+	
+	public static void main(String[] args) {
+		
+		
 		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
 		System.out.println("달을 입력하세요");
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		System.out.printf("%d 월은 %d 일까지 있습니다. \n", month, cal.getMaxDaysOfMonth(month));
 		
-		System.out.printf("%d 월은 %d 일까지 있습니다. \n", month, maxDays[month - 1]);
+		cal.printSampleCalender();
 		scanner.close();
 
 	}
