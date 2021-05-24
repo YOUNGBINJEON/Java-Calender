65a8ad1829d400795fe21c0e5e291ef11bd64ec7
YOUNGBINJEON <sh44972@gmail.com>
2021-05-25 00:11:20 +0900

    [Update] 달력 줄바꿈출력

--- src/yb/calender/Calender.java
+++ src/yb/calender/Calender.java
@@ -39,23 +39,24 @@ public class Calender {
 		}
 		
 		int count = 7 - weekday;
-		
+		int delim = count;
+		// print first line
 		for(int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
 		}
 		System.out.println();
 		
-		for(int i = 1; i<= maxDay; i++) {
+		// print from second line to last
+		count++;
+		for(int i = count; i<= maxDay; i++) {
 			System.out.printf("%3d",i);
-			if(i % 7 == 0) {
+			if(i % 7 == delim) {
 				System.out.println();
 			}
 		}
 		System.out.println();
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9  10 11 12 13 14");
-//		System.out.println(" 15 16 17 18 19 20 21");
-//		System.out.println(" 22 23 24 25 26 27 28");
+		System.out.println();
+
 		
 	}
 
