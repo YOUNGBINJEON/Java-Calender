e6165df1485706cca7bf31e139c0c393b05b9e33
YOUNGBINJEON <sh44972@gmail.com>
2021-05-27 23:09:54 +0900

    [Update] Create menu UI

--- src/yb/calender/Prompt.java
+++ src/yb/calender/Prompt.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Prompt {
 	
+	public void printMenu() {
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록  ");		
+		System.out.println("| 2. 일정 검색  ");		
+		System.out.println("| 3. 달력 보기");		
+		System.out.println("| h. 도움말 q. 종료");		
+		System.out.println("+----------------------+");
+				
+	}
+	
 	private final static String PROMPT ="cal> ";
 	
 	public int parseDay(String week) {
@@ -34,34 +44,60 @@ public class Prompt {
 	
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
-		Calender cal = new Calender();
-		
-		int month = 1;
-		int year = 0;
+		Calender cal = new Calender();	
 		
 		while(true) {
 			
-			System.out.println("년도를 입력하세요(exit:-1)");
-			System.out.print(PROMPT);
-			year = scanner.nextInt();
-			if(year == -1) {
+			String cmd = scanner.next();
+			if(cmd.equals("1")) {
+				cmdRegister();
+			} else if(cmd.equals("2")) {
+				cmdSearch();
+			} else if(cmd.equals("3")) {
+				cmdCal(scanner, cal);
+			} else if(cmd.equals("h")) {
+				printMenu();
+			} else if(cmd.equals("q")) {
 				break;
 			}
-			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			
-			
-			if(month > 12 || month < 1) {
-				System.out.println("잘못된 입력입니다.");
-				continue;
-			}
-			cal.printCalender(year, month);
-			
 		}
+			
+			
 		System.out.println("종료합니다.");
 		
 		scanner.close();
+	}
+
+	private void cmdCal(Scanner s, Calender c) {
+		int month = 1;
+		int year = 0;
+		
+		System.out.println("년도를 입력하세요");
+		System.out.print(PROMPT);
+		year = s.nextInt();
+
+		System.out.println("달을 입력하세요");
+		System.out.print(PROMPT);
+		month = s.nextInt();
+		
+		
+		if(month > 12 || month < 1) {
+			System.out.println("잘못된 입력입니다.");
+			return;
+		}
+		c.printCalender(year, month);
+		
+	}
+	
+
+	private void cmdSearch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void cmdRegister() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public static void main(String[] args) {
